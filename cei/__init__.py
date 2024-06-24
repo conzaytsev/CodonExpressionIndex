07350613132855fa4a06bcf394f2bfc47eb5bffd
conzaytsev <113913648+conzaytsev@users.noreply.github.com>
2024-06-24 17:52:32 +0300

    Update __init__.py

--- cei/__init__.py
+++ cei/__init__.py
@@ -2,7 +2,7 @@
 Python module for analysis of codon influence on protein expression.
 """
 
-__version__ = "0.1.1"
+__version__ = "0.1.0"
 __author__ = 'Konstantin Zaytsev'
 
 import cei.expression_index
