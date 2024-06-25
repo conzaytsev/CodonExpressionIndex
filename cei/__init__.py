cb3ada33abd365ea817fb31199f9ee30d030c2ec
conzaytsev <113913648+conzaytsev@users.noreply.github.com>
2024-06-25 17:28:30 +0300

    Update __init__.py

--- cei/__init__.py
+++ cei/__init__.py
@@ -1,1 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""docstring."""
+
+from expression_index._version import version as __version__
 from .expression_index import ExpressionIndex, CodonProductivity
